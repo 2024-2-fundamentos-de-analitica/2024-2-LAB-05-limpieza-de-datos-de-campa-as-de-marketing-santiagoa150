ffdc0f8fb4467e5a08f5f6244ecae4f06d8a0c21
Santiago Álvarez Muñoz <santiagoa150@gmail.com>
2025-01-07 12:17:03 -0500

    feat: task completed

--- homework/homework.py
+++ homework/homework.py
@@ -3,7 +3,53 @@ Escriba el codigo que ejecute la accion solicitada.
 """
 
 # pylint: disable=import-outside-toplevel
+import pandas as pd
+import zipfile
+import glob
+import os
 
+def read_files(path: str) -> pd.DataFrame:
+    """
+    Esta función lee los archivos .zip de una carpeta y los concatena en un solo DataFrame.
+    """
+
+    df = pd.DataFrame()
+    zips = glob.glob(os.path.join(path, "*"))
+    for zip_name in zips:
+        with zipfile.ZipFile(zip_name, 'r') as z:
+            # List all files in the .zip
+            zip_csv = z.namelist()[0]
+            with z.open(zip_csv) as csv_file:
+                df = pd.concat([df, pd.read_csv(csv_file)], ignore_index=True)
+    return df                  
+
+def generate_clients_csv(df: pd.DataFrame) -> pd.DataFrame:
+    mapped = df[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']].copy()
+    mapped['job'] = mapped['job'].str.replace('.', '').str.replace('-', '_')
+    mapped['education'] = mapped['education'].str.replace('.', '_').replace('unknown', pd.NA)
+    mapped['credit_default'] = mapped['credit_default'].apply(lambda x: 1 if x == 'yes' else 0)
+    mapped['mortgage'] = mapped['mortgage'].apply(lambda x: 1 if x == 'yes' else 0)
+    return mapped
+
+def generate_campaign_csv(df: pd.DataFrame) -> pd.DataFrame:
+    mapped = df[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 'previous_outcome', 'campaign_outcome']].copy()
+    mapped['previous_outcome'] = mapped['previous_outcome'].apply(lambda x: 1 if x == 'success' else 0)
+    mapped['campaign_outcome'] = mapped['campaign_outcome'].apply(lambda x: 1 if x == 'yes' else 0)
+    mapped['last_contact_date'] = pd.to_datetime(
+        df['day'].astype(str) + '-' + df['month'].astype(str) + '-2022',
+        format='%d-%b-%Y'
+    ).astype(str)
+    return mapped
+
+def generate_economics_csv(df: pd.DataFrame) -> pd.DataFrame:
+    mapped = df[['client_id', 'cons_price_idx', 'euribor_three_months']].copy()
+    return mapped
+
+def write_file(df: pd.DataFrame, path: str):
+    """
+    Esta función escribe los DataFrames en archivos csv.
+    """
+    df.to_csv(path, index=False)
 
 def clean_campaign_data():
     """
@@ -49,8 +95,21 @@ def clean_campaign_data():
 
 
     """
+    df = read_files('files/input/')
+    clients = generate_clients_csv(df)
+    campaigns = generate_campaign_csv(df)
+    economics = generate_economics_csv(df)
 
-    return
+    if os.path.exists('files/output'):
+        previus_data = glob.glob(os.path.join('files/output', "*"))
+        for file in previus_data:
+            os.remove(file)
+    else:
+        os.makedirs('files/output')
+
+    write_file(clients, 'files/output/client.csv')
+    write_file(campaigns, 'files/output/campaign.csv')
+    write_file(economics, 'files/output/economics.csv')    
 
 
 if __name__ == "__main__":
